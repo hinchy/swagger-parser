b1a5e90d3c8950067c6e813b2d054e7605dd9efd
Eoin Hinchy <eoinhinchy@gmail.com>
2019-01-09 16:01:11 +0000

    Add new method to generate example schema data

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in swagger.gemspec
 gemspec
 gem 'pry'
+gem 'json_schema-faker', '~> 0.1.0'
--- lib/swagger/schema.rb
+++ lib/swagger/schema.rb
@@ -1,3 +1,5 @@
+require 'json_schema/faker'
+
 module Swagger
   # Represents a Swagger Schema Object, a more deterministic subset of JSON Schema.
   # @see https://github.com/wordnik/swagger-spec/blob/master/versions/2.0.md#schema-object- Schema Object
@@ -11,6 +13,25 @@ module Swagger
     def initialize(hash, default = nil)
       super
       attach_to_children
+    end
+
+    def generate_example
+      schema = clone
+      if schema.key?('$ref')
+        key = schema.delete('$ref').split('/').last
+        model = root.definitions[key].generate_example
+        schema.merge!(model["properties"])
+      end
+
+      count = 0
+      until schema.refs_resolved?
+        #puts count
+        fail 'Could not resolve non-remote $refs 5 cycles - circular references?' if count >= 5
+        schema.resolve_refs_example
+        count += 1
+      end
+
+      schema.to_hash
     end
 
     def parse
@@ -48,6 +69,21 @@ module Swagger
       model = root.definitions[key].parse
       self.merge!(model)
     end
+    
+    
+    
+    def resolve_refs_example
+      children.each do |child|
+        child.resolve_refs_example if child.is_a?(Swagger::Schema)
+      end
+      key = self.delete('$ref')
+      return if key.nil? || remote_ref?(key)
+      key = key.split('/').last
+      model = root.definitions[key].parse
+      
+      json_schema = JsonSchema.parse!(model)
+      self.merge!(JsonSchema::Faker.new(json_schema).generate)
+    end    
 
     def refs_resolved?
       return true if refs.nil?
